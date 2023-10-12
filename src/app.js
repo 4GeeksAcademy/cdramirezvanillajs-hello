9d8422ade00208c1b2cf6ab65392f2bdac1a9358
Borknagar <140437008+Borknagar@users.noreply.github.com>
2023-10-12 15:49:34 +0000

    DomGenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "yours", "his", "her"];
+  let adj = ["great", "intelligent", "happy", "sad", "fantastic", "terrific"];
+  let noun = ["captain", "astronaut", "clown", "cheff", "farmer"];
+  let extension = [".es", ".com", ".gov", ".org", ".net"];
+
+  let pronounIn = Math.floor(Math.random() * pronouns.length);
+  let adjIn = Math.floor(Math.random() * adj.length);
+  let nounIn = Math.floor(Math.random() * noun.length);
+  let extensionIn = Math.floor(Math.random() * extension.length);
+
+  let domain =
+    pronouns[pronounIn] + adj[adjIn] + noun[nounIn] + extension[extensionIn];
+
+  let dominio = document.getElementById("dominio");
+  dominio.innerHTML = domain;
+
+  console.log("Cualquier string");
 };
